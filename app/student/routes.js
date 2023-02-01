3d4009dd00e8741de58d06893dff8d7af1e121c8
cDavid2020 <116908160+cDavid2020@users.noreply.github.com>
2023-02-01 14:37:24 -0600

    Add notes: validation vs unit testing

--- app/student/routes.js
+++ app/student/routes.js
@@ -17,3 +17,15 @@ router.get("/", (_, res) => {
 });
 
 export default router;
+
+/*
+VALIDATION same as UNIT TESTING?
+
+No, validation and unit testing are not the same in JavaScript programming.
+
+Validation refers to the process of checking the validity of user input data, such as verifying if an email address is in the correct format. This helps to ensure that the data entered by the user is valid and meets the required criteria before being processed.
+
+Unit testing, on the other hand, is a software testing technique in which individual units or components of the code are tested in isolation to ensure that they are functioning correctly. This helps to catch bugs early in the development process and makes it easier to maintain the code over time.
+
+In summary, validation focuses on the correctness of user input data, while unit testing focuses on the correctness of code implementation.
+ */
